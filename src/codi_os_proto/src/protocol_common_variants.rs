ae2cdf0ba9eca7247b558d82cb28c2c036ac35c5
Dom Rodriguez <shymega@shymega.org.uk>
2021-11-13 16:36:20 +0000

    Remove generated ProtoBuf code from build and Git
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/codi_os_proto/src/protocol_common_variants.rs
+++
@@ -1,6 +0,0 @@
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
-#[repr(i32)]
-pub enum CoDiVariant {
-    Stock = 0,
-    Codios = 1,
-}
--- src/codi_os_proto/src/protocol_flashing.rs
+++
@@ -1,7 +0,0 @@
-#[derive(Clone, PartialEq, ::prost::Message)]
-pub struct QueryFlashRequest {
-    #[prost(int32, tag="1")]
-    pub firmware_version: i32,
-    #[prost(enumeration="super::protocol_common_variants::CoDiVariant", tag="2")]
-    pub firmware_variant: i32,
-}
