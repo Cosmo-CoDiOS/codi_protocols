d4ea408e805f84baf479df351a736b6159a5a9ff
Dom Rodriguez <shymega@shymega.org.uk>
2022-03-01 18:22:34 +0000

    Fix warnings on serial port module
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/codi_proto_common/src/std_serial_handler/mod.rs
+++ src/codi_proto_common/src/std_serial_handler/mod.rs
@@ -1,7 +1,5 @@
 //! `std` serial port handling crate.
 
-use serialport::SerialPortBuilder;
-
 /// Open serial
 pub fn open_serial_conn(device_node: String) {    
     let ports = serialport::available_ports()
@@ -11,12 +9,8 @@ pub fn open_serial_conn(device_node: String) {
         println!("{:?}", p);
     }
 
-    let mut port = serialport::new("/dev/ttyS1", 115_200)
+    let _port = serialport::new(&device_node, 115_200)
         .timeout(::std::time::Duration::from_millis(1000))
         .open()
-        .unwrap();
-
-    port.write("ACCEPT TRANSMISSION"
-               .as_bytes())
         .unwrap();
 }
