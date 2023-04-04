0e07404c009e3c762b67cfa005c47d98fec4d91e
Dom Rodriguez <shymega@shymega.org.uk>
2023-04-04 23:07:43 +0100

    Refactor `codi_protocol_common` to use traits
    Combined with feature flags, we can aim to add support for
    `embedded-hal`, meaning we can target the CoDiOS firmware.
    
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/codi_protocol_common/src/lib.rs
+++ src/codi_protocol_common/src/lib.rs
@@ -1,5 +1,5 @@
 //! This crate handles UART communication between the `CoDi` chip and the host ROM.
-#![cfg_attr(all(target_arch = "arm"), no_std)]
+#![cfg_attr(all(feature = "std"), no_std)]
 #![deny(
     warnings,
     missing_copy_implementations,
@@ -17,5 +17,17 @@
     variant_size_differences
 )]
 
+#[cfg(not(feature = "std"))]
+pub mod no_std_serial_port_manager;
+
 #[cfg(feature = "std")]
-pub mod std_serial_handler;
+pub mod std_serial_port_manager;
+
+// Compiles with both `no_std` and `std`.
+
+use serialport::SerialPort;
+
+pub trait SerialPortTrait {
+    fn open_serial_port(&mut self, dev: &str, baud_rate: u32);
+    fn read_packet(&self);
+}
