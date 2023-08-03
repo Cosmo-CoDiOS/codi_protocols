31a80736bfaa85dfd1f25a72bff8d51ae4090b9a
Dom Rodriguez <shymega@shymega.org.uk>
2023-08-03 20:55:25 +0100

    Update protobuf generated code

--- src/codi_boot_protocol/src/protocol_codi_boot_flashing.rs
+++ src/codi_boot_protocol/src/protocol_codi_boot_flashing.rs
@@ -1,12 +1,14 @@
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
-pub struct QueryFlashCommand {
-}
+pub struct QueryFlashCommand {}
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct QueryFlashResponse {
-    #[prost(string, tag="1")]
+    #[prost(string, tag = "1")]
     pub firmware_version: ::prost::alloc::string::String,
-    #[prost(enumeration="super::protocol_common_variants::CoDiVariant", tag="2")]
+    #[prost(
+        enumeration = "super::protocol_common_variants::CoDiVariant",
+        tag = "2"
+    )]
     pub firmware_variant: i32,
 }
