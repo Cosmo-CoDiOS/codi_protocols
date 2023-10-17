acc62166b202642dbb7af177ca1c40841ecac400
Dom Rodriguez <shymega@shymega.org.uk>
2023-10-17 21:05:46 +0100

    Run formatter on codebase (again)

--- src/codi_stock_protocol/src/connection.rs
+++ src/codi_stock_protocol/src/connection.rs
@@ -1,7 +1,7 @@
 //! This module is for the stock `CoDi` `Connection`
 
-use crate::StockCoDiPacket;
 use crate::serial_manager::StockCoDiSerialPortManager;
+use crate::StockCoDiPacket;
 
 #[allow(missing_debug_implementations)]
 /// `StockCoDiConnection` holds packets from/to `CoDi`, and the `StockCoDiSerialPortManager`.
