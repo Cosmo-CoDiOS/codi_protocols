5abf42eb3da1baadbe80a90e0d8fdb5673632540
Dom Rodriguez <shymega@shymega.org.uk>
2023-12-09 16:52:25 +0000

    fix(Flake): Use latest rustc-stable

--- flake.nix
+++ flake.nix
@@ -2,30 +2,46 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     naersk.url = "github:nix-community/naersk";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    nixpkgs-mozilla = {
+      url = "github:mozilla/nixpkgs-mozilla";
+      flake = false;
+    };
   };
 
-  outputs = { self, flake-utils, naersk, nixpkgs }:
+  outputs = { self, flake-utils, naersk, nixpkgs, nixpkgs-mozilla }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = (import nixpkgs) {
           inherit system;
+
+          overlays = [
+            (import nixpkgs-mozilla)
+          ];
         };
 
-        naersk' = pkgs.callPackage naersk {};
+        toolchain = (pkgs.rustChannelOf {
+          rustToolchain = ./rust-toolchain;
+          sha256 = "sha256-PjvuouwTsYfNKW5Vi5Ye7y+lL7SsWGBxCtBOOm2z14c=";
+        }).rust;
+
+        naersk' = pkgs.callPackage naersk {
+          cargo = toolchain;
+          rustc = toolchain;
+        };
 
       in rec {
         # For `nix build` & `nix run`:
         defaultPackage = naersk'.buildPackage {
           src = ./.;
           nativeBuildInputs = with pkgs; [ pkg-config cmake ] ;
-          buildInputs = with pkgs; [ protobufc protobuf systemd.dev ];
+          buildInputs = with pkgs; [ protobuf protobufc systemd.dev ];
         };
 
-        # For `nix develop`:
+        # For `nix develop` (optional, can be skipped):
         devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [ rustc cargo pkg-config cmake ];
-          buildInputs = with pkgs; [ protobufc protobuf systemd.dev ];
+          nativeBuildInputs = [ toolchain ] ++ (with pkgs; [ rustc cargo pkg-config cmake ]);
+          buildInputs = with pkgs; [ protobuf protobufc systemd.dev ];
         };
       }
     );
